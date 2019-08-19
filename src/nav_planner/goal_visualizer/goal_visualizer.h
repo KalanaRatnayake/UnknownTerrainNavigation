e29e8d015e256e1df72817981cea5a5ef8c872da
KalanaRatnayake <kalana.15@cse.mrt.ac.lk>
2019-08-19 11:58:37 +0530

    created goal identiffier class and Goal Visualizer class

---
+++ src/nav_planner/goal_visualizer/goal_visualizer.h
@@ -0,0 +1,16 @@
+#include <vector>
+#include <octomap/octomap.h>
+#include <octomap/ColorOcTree.h>
+#include <cmath>
+
+class goal_visualizer{
+    private:
+        octomap::ColorOcTree tree;
+
+    
+    public:
+        void goal_visualizer(float inputResolution);
+        void update_cluster_centers(std::vector<octomap::point3d> inCenterPointsArray);
+        void update_nearest_cluster(octomap::point3d inCluster);
+        void get_tree(octomap::ColorOcTree outTree);
+}
