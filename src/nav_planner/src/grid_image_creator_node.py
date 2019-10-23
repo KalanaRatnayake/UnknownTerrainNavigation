434e0553409be192ae686b8e4cbef3fa73cfafcd
KalanaRatnayake <kalana.15@cse.mrt.ac.lk>
2019-10-23 23:19:20 +0530

    updated grid related issues on global_path_planner_node and grid_image_creator_node

--- src/nav_planner/src/grid_image_creator_node.py
+++ src/nav_planner/src/grid_image_creator_node.py
@@ -4,6 +4,8 @@ import rospy
 import numpy as np
 import cv2
 from nav_planner.msg import gridMap, gridRow, gridPoint
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
 
 ROW = 400 
 COL = 400
@@ -12,43 +14,50 @@ initlock = False
 proclock = False
 pathlock = False
 
-grid = np.zeros([ROW, COL, 3], np.int8)
+class grid_image_creator:
 
-# Description of the Grid- {1--> not occupied} {0--> occupied} 
+    # Description of the Grid- {1--> not occupied} {0--> occupied} 
 
-def save():
-    name = "/FYP/src/nav_planner/grid_Images/"+ str(index) +".jpg"
-    cv2.imwrite(name, grid)
-    index += 1
+    def __init__(self):
+        self.image_pub = rospy.Publisher("gridMap",Image)
+        self.gridM_sub = rospy.Subscriber("grid", gridMap, self.callbackGrid)
+        self.bridge = CvBridge()
 
-def callbackGrid(msg):
-    for i in range(0, ROW):
-        array = msg.grid[i]
+    def callbackGrid(self, msg):
+        grid = np.zeros([ROW, COL, 3], np.int8)
 
-        for j in range (0, COL):
-            point = array.row[j]
+        for i in range(0, ROW):
+            array = msg.grid[i]
 
-            #initial grid is saved as BLUE channel
-            if (point.init==0):
-                grid[i][j][0] = 255
+            for j in range (0, COL):
+                point = array.row[j]
 
-            #processed grid (with padding) is saved as GREEN channel
-            if (point.proc==0):
-                grid[i][j][1] = 255
+                #initial grid is saved as BLUE channel
+                if (point.init==0):
+                    self.grid[i][j][0] = 255
 
-            #path is saved as RED channel
-            if (point.path==0):
-                grid[i][j][2] = 255
-                
-    save()
+                #processed grid (with padding) is saved as GREEN channel
+                if (point.proc==0):
+                    self.grid[i][j][1] = 255
 
-    
-def grid_image_creator():
+                #path is saved as RED channel
+                if (point.path==0):
+                    self.grid[i][j][2] = 255
+                    
+        name = "/FYP/src/nav_planner/grid_Images/"+ str(index) +".jpg"
+        cv2.imwrite(name, self.grid)
+        index += 1
+
+        try:
+            self.image_pub.publish(self.bridge.cv2_to_imgmsg(grid, "bgr8"))
+        except CvBridgeError as e:
+            print(e)
+
+        
+def main():
+    creator = grid_image_creator()
     rospy.init_node('Grid_Image_Creator')
-
-    rospy.Subscriber("grid", gridMap, callbackGrid)
-
     rospy.spin()
 
 if __name__ == '__main__':
-    grid_image_creator()
+    main()
